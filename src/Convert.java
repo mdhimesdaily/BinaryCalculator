71bbcd6837b205660475b6f716e54079492d412c
mdhimesdaily <mdhimes@bsu.edu>
2019-09-19 14:09:50 -0400

    Updated convert methods to accept a string for testing before UI is built.

--- src/Convert.java
+++ src/Convert.java
@@ -1,12 +1,22 @@
 import java.util.Scanner;
 
-public class Convert {
-    public static void main(String[] args)
-    {
+class Convert {
+
+    Integer ConverttoDecimal(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a binary string");
+        System.out.println("Enter a binary number");
         String binaryString=scanner.nextLine();
         int decimal=Integer.parseInt(binaryString,2);
         System.out.println(decimal);
+        return decimal;
+    }
+
+    Integer ConverttoBinary(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a decimal number");
+        Integer decimalNumber = scanner.nextInt();
+        int binary= Integer.parseInt(Integer.toBinaryString(decimalNumber));
+        System.out.println(binary);
+        return binary;
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Convert converter = new Convert();
+        converter.ConverttoDecimal();
+        converter.ConverttoBinary();
+    }
+}
